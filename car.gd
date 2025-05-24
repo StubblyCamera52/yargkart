4ca9289a9251673c85244797320ba7fd99600ab8
TaelsDaFoox <bazmulligan@gmail.com>
2025-05-24 11:45:22 -0700

    started car script

--- car.gd
+++ car.gd
@@ -3,12 +3,15 @@ extends CharacterBody3D
 @export var engine_force_value: float = 30.0
 @export var steer_speed: float = 1.5
 @export var steer_limit: float = 0.5
+@export var decel: float = 30.0
+@export var gravity: float = 30.0
 
 var player: int
 var input
 var steer_target := 0
 var engine
 var steer := 0
+var player_vel := 0.0
 
 var previous_speed = velocity.length()
 
@@ -26,10 +29,18 @@ func _physics_process(delta: float) -> void:
 		engine = engine_force_value
 	else:
 		engine = 0.0
-		
-	steer = move_toward(steer, steer_target, delta*steer_speed)
 	
-	velocity 
+	if is_on_floor():
+		pass
+	else:
+		velocity.y-=gravity*delta
+	steer = move_toward(steer, steer_target, delta*steer_speed)
+	rotation.y+=steer_target*delta
+	player_vel += engine*delta
+	player_vel = move_toward(player_vel, 0, decel*delta)
+	velocity.x = cos(rotation.y)*player_vel*delta
+	velocity.z = -sin(rotation.y)*player_vel*delta
+	move_and_slide()
 	
 	previous_speed = 0
 	
