84d865f1903565e081da18ab6099cace710154f7
IvyYoshi <calvin.e.mcfarlane@gmail.com>
2025-05-24 12:59:43 -0700

    Update car.gd

--- car.gd
+++ car.gd
@@ -5,6 +5,8 @@ extends CharacterBody3D
 @export var steer_limit: float = 0.5
 @export var decel: float = 30.0
 @export var gravity: float = 30.0
+@export var speed_limit: float = 4500.0 # miles per hour lmao
+@export var wheel_rotate_speed: float = 0.01
 
 var item_manager
 
@@ -14,10 +16,37 @@ var steer_target := 0
 var engine
 var steer := 0
 var player_vel := 0.0
+@onready var wheelFL = $FLWheelModel
+@onready var wheelFR = $FrWheelModel
+@onready var wheelBL = $BLWheelModel
+@onready var wheelBR = $BRWheelModel
+var wheels = [wheelFL, wheelFR, wheelBL, wheelBR]
 
 var previous_speed = velocity.length()
 
+<<<<<<< Updated upstream
 func init(player_id: int, device_id: int, item_manager_node) -> void:
+=======
+func rotate_wheels_x(dir: float, delta: float) -> void:
+	wheelFL.rotation.x = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelFR.rotation.x = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelBL.rotation.x = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelBR.rotation.x = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	
+func rotate_wheels_y(dir: float, delta: float) -> void:
+	wheelFL.rotation.y = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelFR.rotation.y = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelBL.rotation.y = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelBR.rotation.y = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	
+func rotate_wheels_z(dir: float, delta: float) -> void:
+	wheelFL.rotation.z = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelFR.rotation.z = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelBL.rotation.z = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+	wheelBR.rotation.z = move_toward(player_vel, dir, delta*wheel_rotate_speed)
+
+func init(player_id: int, device_id: int) -> void:
+>>>>>>> Stashed changes
 	player = player_id
 	
 	item_manager = item_manager_node
@@ -26,7 +55,10 @@ func init(player_id: int, device_id: int, item_manager_node) -> void:
 
 func _physics_process(delta: float) -> void:
 	var gas_input_amount = input.get_action_strength("Accelerate")
-	steer_target = input.get_axis("Steer_Right", "Steer_Left")*2
+	steer_speed = player_vel*0.005
+	if steer_speed > 10:
+		steer_speed = 10
+	steer_target = input.get_axis("Steer_Right", "Steer_Left")*steer_speed
 	steer_target *= steer_limit
 	
 	if input.is_action_pressed("Accelerate"):
@@ -40,12 +72,19 @@ func _physics_process(delta: float) -> void:
 		pass
 	else:
 		velocity.y-=gravity*delta
+	
 	steer = move_toward(steer, steer_target, delta*steer_speed)
 	rotation.y+=steer_target*delta
-	player_vel += engine*delta
+	if is_on_floor():
+		player_vel += engine*delta
+		if player_vel > speed_limit:
+			player_vel = speed_limit
 	player_vel = move_toward(player_vel, 0, decel*delta)
 	velocity.x = cos(rotation.y)*player_vel*delta
 	velocity.z = -sin(rotation.y)*player_vel*delta
+	
+	rotate_wheels_x(engine*delta, delta)
+	
 	move_and_slide()
 	
 	#items
@@ -55,3 +94,11 @@ func _physics_process(delta: float) -> void:
 	previous_speed = 0
 	
 	previous_speed = velocity.length()
+	
+	# to implement:
+	# speed cap ✔
+	# do not accel if in air ✔
+	# make turning, like, good ✖
+	# make turning, like, passable ✔
+	# wheel crap
+	# make turning not stop immediately
