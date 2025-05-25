85008db4230eadcb14582671ef77f2b7fd21b063
Gavan <gavanbess@gmail.com>
2025-05-24 17:51:23 -0700

    remove camera

---
+++ camera_3d.gd
@@ -0,0 +1,19 @@
+extends Camera3D
+
+@onready var target = $".."/SpringArm3D/Node3D
+
+var camera_rot_target: float = 0
+
+func shake(time: float, strength: float) -> void:
+	var elapsed_time := 0.0
+
+	while elapsed_time < time:
+		var actual_strength = lerpf(strength, 0, elapsed_time/time)
+		var offset := Vector3(randf_range(-actual_strength, actual_strength), randf_range(-actual_strength, actual_strength), 0.0)
+		
+		transform.origin += offset
+		elapsed_time += get_process_delta_time()
+		await get_tree().process_frame
+
+func _physics_process(delta: float) -> void:
+	global_position = target.global_position
